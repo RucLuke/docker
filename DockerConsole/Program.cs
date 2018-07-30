2439bb0dcd2ae47dbf1c94f56d1db2cfaf1fe760
RucLuke <lixinkuo@ruc.edu.cn>
2018-07-30 18:25:52 +0800

    Revert "Revert "project init""
    This reverts commit a3df27b1401294efac744e5d0bca4bfabd5e901d.

---
+++ DockerConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+namespace DockerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World From Docker!");
+            System.Threading.Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
